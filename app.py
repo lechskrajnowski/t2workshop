1c2b88c0a94f67e3bb18894b4ff251dcdde36a04
lechskrajnowski <57195908+lechskrajnowski@users.noreply.github.com>
2025-06-04 13:09:36 +0000

    terraform 5

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+# Greetings!
+# This is a simple Flask app for the Global Azure Workshop 2025.
+# It allows users to add comments from the browser.
+
+from flask import Flask, request, render_template_string, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+comments = []
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Hello, Global Azure Workshop 2025!"
+    if request.method == 'POST':
+        comment = request.form.get('comment')
+        if comment:
+            comments.append(comment)
+        return redirect(url_for('home'))
+    return render_template_string('''
+        <h1>Hello, Global Azure Workshop 2025!</h1>
+        <h2>Greetings!</h2>
+        <form method="post">
+            <input name="comment" placeholder="Add your comment" required>
+            <button type="submit">Add Comment</button>
+        </form>
+        <h3>Comments:</h3>
+        <ul>
+        {% for c in comments %}
+            <li>{{ c }}</li>
+        {% endfor %}
+        </ul>
+    ''', comments=comments)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
