07216de48fe4b1ab3dfde917cc1afa4493085bea
lechskrajnowski <57195908+lechskrajnowski@users.noreply.github.com>
2025-06-04 11:51:46 +0000

    terraform final

--- main.tf
+++ main.tf
@@ -36,3 +36,25 @@ resource "azurerm_linux_web_app" "example" {
   service_plan_id     = azurerm_service_plan.example.id
   site_config {}
 }
+
+
+  update-app-service:
+    name: Update App Service with Latest Image
+    runs-on: ubuntu-latest
+    needs: deploy-infra
+
+    steps:
+      - name: 'Azure login'
+        uses: azure/login@v1
+        with:
+          client-id: ${{ secrets.AZURE_CLIENT_ID }}
+          tenant-id: ${{ secrets.AZURE_TENANT_ID }}
+          subscription-id: ${{ secrets.AZURE_SUBSCRIPTION_ID }}
+        
+      - name: Update App Service
+        run: |
+          az webapp config container set \
+            --name lechs-webapp-workshop \
+            --resource-group rg-lechs \
+            --docker-custom-image-name ${{ secrets.ACR_LOGIN_SERVER }}/example-webapp:latest \
+            --docker-registry-server-url https://${{ secrets.ACR_LOGIN_SERVER }}
